a3b761d9c7bd8c0a2d210a65948acbab61b63ce4
TiagoSilva22102750 <a22102750@alunos.ulht.pt>
2023-03-24 18:31:23 +0000

    Sera que e desta? 5

--- src/pt/ulusofona/cm/kotlin/challenge/exceptions/AlterarPosicaoException.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/exceptions/AlterarPosicaoException.kt
@@ -1,3 +1,5 @@
 package pt.ulusofona.cm.kotlin.challenge.exceptions
 
-class AlterarPosicaoException : Exception("Voce ja se encontra nesta posicao. Por favor tente outra.")
+class AlterarPosicaoException(message: String): Exception(message) {
+
+}
--- src/pt/ulusofona/cm/kotlin/challenge/models/Posicao.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Posicao.kt
@@ -5,7 +5,7 @@ import pt.ulusofona.cm.kotlin.challenge.exceptions.AlterarPosicaoException
 class Posicao(var x: Int, var y: Int) {
     fun alterarPosicaoPara(x: Int, y: Int) {
         if (this.x == x && this.y == y) {
-            throw AlterarPosicaoException()
+            throw AlterarPosicaoException("Você já se encontra nesta posição. Por favor tente outra.")
         }
         this.x = x
         this.y = y
